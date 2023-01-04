63275b8d241579b8faa4a6b31b48a4824d5ac0da
yenilikci <57464067+yenilikci@users.noreply.github.com>
2023-01-04 10:42:45 +0300

    Add Advertisement entity

---
+++ AdvertisementApp.Entities/Advertisement.cs
@@ -0,0 +1,10 @@
+namespace AdvertisementApp.Entities
+{
+    public class Advertisement
+    {
+        public string Title { get; set; }
+        public bool Status { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
