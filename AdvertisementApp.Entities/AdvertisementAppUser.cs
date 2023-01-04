d2f74ef425b5256d51aa621ce5982ad732b6ebe3
yenilikci <57464067+yenilikci@users.noreply.github.com>
2023-01-04 10:49:24 +0300

    Add AdvertisementUser entity

---
+++ AdvertisementApp.Entities/AdvertisementAppUser.cs
@@ -0,0 +1,13 @@
+namespace AdvertisementApp.Entities
+{
+    public class AdvertisementAppUser : BaseEntity
+    {
+        public int AdvertisementId { get; set; }
+        public int AppUserId { get; set; }
+        public int AdvertisementAppUserStatusId { get; set; }
+        public int MilitaryStatusId { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int WorkExperience { get; set; }
+        public string CVPath { get; set; }
+    }
+}
