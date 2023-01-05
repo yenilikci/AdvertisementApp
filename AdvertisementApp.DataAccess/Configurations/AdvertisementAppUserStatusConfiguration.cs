023819d1b0fa5c521c0eb86ef5ba2eb8bdc2f627
yenilikci <m.melihcelik017@outlook.com>
2023-01-05 23:48:22 +0300

    add AdvertisementAppUserStatusConfiguration

---
+++ AdvertisementApp.DataAccess/Configurations/AdvertisementAppUserStatusConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AdvertisementApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.DataAccess.Configurations
+{
+    internal class AdvertisementAppUserStatusConfiguration : IEntityTypeConfiguration<AdvertisementAppUserStatus>
+    {
+        public void Configure(EntityTypeBuilder<AdvertisementAppUserStatus> builder)
+        {
+            builder.Property(x => x.Definition).HasMaxLength(300).IsRequired();
+        }
+    }
+}
