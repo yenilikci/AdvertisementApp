29bf5d9b76ef69d300a6f4b7c0e87429743c5d6f
yenilikci <m.melihcelik017@outlook.com>
2023-01-05 23:59:47 +0300

    Add AppUserConfiguration

--- AdvertisementApp.Entities/AppUser.cs
+++ AdvertisementApp.Entities/AppUser.cs
@@ -2,6 +2,8 @@ namespace AdvertisementApp.Entities
 {
     public class AppUser : BaseEntity
     {
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
